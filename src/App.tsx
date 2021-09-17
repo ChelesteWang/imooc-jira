b0b89af3b606adc7cf35b312ed2b7b412dddd06d
yuqing521 <1581446178@qq.com>
2021-09-17 08:35:29 +0800

    feat: add app.tsx  annotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./App.css";
+
+// 鉴权
 import { useAuth } from "context/auth-context";
+// 容错
 import { ErrorBoundary } from "components/error-boundary";
+// 降级
 import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 
+// 懒加载
 const AuthenticatedApp = React.lazy(() => import("authenticated-app"));
 const UnauthenticatedApp = React.lazy(() => import("unauthenticated-app"));
 
